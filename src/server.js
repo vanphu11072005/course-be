102c3a54c1d0db8d2d394b3354c742e168f11f31
phuphan <phuphan@gmail.com>
2025-10-16 19:19:52 +0700

    deploy be

--- src/server.js
+++ src/server.js
@@ -21,13 +21,13 @@ const port = AppConfig.port;
 
 // ===== Socket.IO =====
 const io = new Server(server, {
-  cors: "http://localhost:5173", // replace with your React app URL
+  cors: "https://webcoure-qaoiq4bwy-phucit226s-projects.vercel.app", // replace with your React app URL
 });
 
 db.sequelize.sync({ force: false });
 app.use(
   cors({
-    origin: "http://localhost:5173", // replace with your React app URL
+    origin: "https://webcoure-qaoiq4bwy-phucit226s-projects.vercel.app", // replace with your React app URL
     credentials: true,
   })
 ); // Enable CORS for all routes
